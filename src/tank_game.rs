a408eb9c7a24f3f31d2d24156b923bb96ed7ac05
Julio Gutierrez <juliog@knights.ucf.edu>
2022-01-08 20:31:11 -0500

    Now using the ground texture

--- src/tank_game.rs
+++ src/tank_game.rs
@@ -3,7 +3,7 @@ use std::cell::RefCell;
 use std::convert::TryInto;
 use std::rc::Rc;
 
-use crate::texture::{load_image_as_texture, create_rgba_texture_from_array_buffer_view};
+use crate::texture::{load_image_as_texture, create_rgba_texture_from_array_buffer_view, create_rgba_texture_from_u8_array};
 
 use super::buffer::{create_square_buffer, create_texture_buffer};
 use super::matrix::Mat4;
@@ -30,13 +30,16 @@ struct TankGameFlyweight {
     projection_matrix_uniform: WebGlUniformLocation,
     model_view_matrix_uniform: WebGlUniformLocation,
     texture_sampler_uniform: WebGlUniformLocation,
+    mask_sampler_uniform: WebGlUniformLocation,
     square_buffer: WebGlBuffer,
     texture_buffer: WebGlBuffer,
     projection: Mat4,
     model_view: Mat4,
     vertex_array_object: WebGlVertexArrayObject,
-    foreground_texture_buffer: js_sys::Uint8Array,
-    foreground_texture: WebGlTexture,
+    foreground_mask_buffer: js_sys::Uint8Array,
+    foreground_mask_texture: WebGlTexture,
+    foreground_texture: Rc<WebGlTexture>,
+    one_mask: WebGlTexture,
     game_state: GameState
 }
 
@@ -78,6 +81,7 @@ fn initialize(canvas: &HtmlCanvasElement, gl: &WebGl2RenderingContext) -> Result
     console::log_1(&"Initializing tank game".into());
 
     let background_texture = load_image_as_texture(&gl, "assets/tankgame/background.jpg")?;
+    let foreground_texture = load_image_as_texture(&gl, "assets/tankgame/ground.jpg")?;
 
     let vertex_shader_source = r##"
 attribute vec4 aVertexPosition;
@@ -99,12 +103,18 @@ precision mediump float;
 
 varying highp vec2 vTextureCoord;
 
-uniform sampler2D uSampler;
+uniform sampler2D uTextureSampler;
+uniform sampler2D uMaskSampler;
 
 void main(void) {
-    vec4 color = texture2D(uSampler, vTextureCoord);
+    vec4 color = texture2D(uTextureSampler, vTextureCoord);
+    vec4 mask = texture2D(uMaskSampler, vTextureCoord);
+
+    color = color * mask;
+
     if (color.a < 0.1)
         discard;
+
     gl_FragColor = color;
 }
     "##;
@@ -134,8 +144,13 @@ void main(void) {
 
     let texture_sampler_uniform = gl.get_uniform_location(
         &program,
-        "uSampler")
+    "uTextureSampler")
         .ok_or_else(|| String::from("Could not get texture sampler uniform location"))?;
+
+    let mask_sampler_uniform = gl.get_uniform_location(
+        &program,
+    "uMaskSampler"
+    ).ok_or_else(|| String::from("Could not get mask sampler uniform location"))?;
 
     let square_buffer = create_square_buffer(&gl)?;
     let texture_buffer = create_texture_buffer(&gl)?;
@@ -232,20 +247,25 @@ void main(void) {
     }
 
     let buffer_size = (canvas.client_width()*canvas.client_height()*4) as u32;
-    let mut foreground_texture_buffer = js_sys::Uint8Array::new_with_length(buffer_size);
+    let mut foreground_mask_buffer = js_sys::Uint8Array::new_with_length(buffer_size);
 
-    generate_foreground_texture_buffer(
-        &mut foreground_texture_buffer,
+    generate_foreground_mask_buffer(
+        &mut foreground_mask_buffer,
         &game_state.terrain_contour,
         canvas.client_width() as u32,
         canvas.client_height() as u32
     );
 
-    let foreground_texture = create_rgba_texture_from_array_buffer_view(
+    let foreground_mask_texture = create_rgba_texture_from_array_buffer_view(
         gl,
         canvas.client_width(),
         canvas.client_height(),
-        &mut foreground_texture_buffer
+        &mut foreground_mask_buffer
+    )?;
+
+    let mask_array = [255, 255, 255, 255];
+    let one_mask = create_rgba_texture_from_u8_array(
+        &gl, 1, 1, &mask_array
     )?;
 
     Ok(TankGameFlyweight {
@@ -258,12 +278,15 @@ void main(void) {
         square_buffer: square_buffer,
         texture_buffer: texture_buffer,
         texture_sampler_uniform: texture_sampler_uniform,
+        mask_sampler_uniform: mask_sampler_uniform,
         vertex_position_attrib: vertex_position_attrib,
         vertex_texture_attrib: vertex_texture_attrib,
         vertex_array_object: vertex_array_object,
-        foreground_texture: foreground_texture,
-        foreground_texture_buffer: foreground_texture_buffer,
-        game_state: game_state
+        foreground_mask_texture: foreground_mask_texture,
+        foreground_mask_buffer: foreground_mask_buffer,
+        one_mask: one_mask,
+        game_state: game_state,
+        foreground_texture: foreground_texture
     })
 }
 
@@ -281,7 +304,7 @@ fn generate_terrain_contour(
     }
 }
 
-fn generate_foreground_texture_buffer(
+fn generate_foreground_mask_buffer(
     buffer: &mut js_sys::Uint8Array,
     contour: &js_sys::Float32Array,
     width: u32,
@@ -323,8 +346,6 @@ fn render(gl: &WebGl2RenderingContext, game: &TankGameFlyweight) {
 
     gl.clear_color(0.0, 0.0, 0.0, 1.0);
     gl.clear_depth(1.0);
-    gl.enable(WebGl2RenderingContext::BLEND);
-    gl.blend_func(WebGl2RenderingContext::SRC_COLOR, WebGl2RenderingContext::DST_COLOR);
     gl.enable(WebGl2RenderingContext::DEPTH_TEST);
     gl.depth_func(WebGl2RenderingContext::LEQUAL);
     gl.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT | WebGl2RenderingContext::DEPTH_BUFFER_BIT);
@@ -343,11 +364,15 @@ fn render(gl: &WebGl2RenderingContext, game: &TankGameFlyweight) {
     );
 
     gl.bind_vertex_array(Some(&game.vertex_array_object));
+
     gl.active_texture(WebGl2RenderingContext::TEXTURE0);
     gl.bind_texture(WebGl2RenderingContext::TEXTURE_2D, Some(&game.background_texture));
-
     // Tell the shader we bound the texture to texture unit 0
     gl.uniform1i(Some(&game.texture_sampler_uniform), 0);
+
+    gl.active_texture(WebGl2RenderingContext::TEXTURE1);
+    gl.bind_texture(WebGl2RenderingContext::TEXTURE_2D, Some(&game.one_mask));
+    gl.uniform1i(Some(&game.mask_sampler_uniform), 1);
 
     {
         let offset = 0;
@@ -361,6 +386,12 @@ fn render(gl: &WebGl2RenderingContext, game: &TankGameFlyweight) {
 
     gl.active_texture(WebGl2RenderingContext::TEXTURE0);
     gl.bind_texture(WebGl2RenderingContext::TEXTURE_2D, Some(&game.foreground_texture));
+    gl.uniform1i(Some(&game.texture_sampler_uniform), 0);
+
+    gl.active_texture(WebGl2RenderingContext::TEXTURE1);
+    gl.bind_texture(WebGl2RenderingContext::TEXTURE_2D, Some(&game.foreground_mask_texture));
+    gl.uniform1i(Some(&game.mask_sampler_uniform), 1);
+
     {
         let offset = 0;
         let vertex_count = 4;
