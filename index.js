27415beb7aacf18b67638cf9d1090f37b0add8c5
Julio Gutierrez <juliog@knights.ucf.edu>
2022-02-19 00:58:20 -0500

    Migrate game to new repo

--- index.js
+++ index.js
@@ -6,23 +6,3 @@ import '@fortawesome/fontawesome-free/js/brands';
 import { greet, tank_game } from './pkg';
 greet();
 
-function hashHandler() {
-  navigate();
-}
-
-window.addEventListener('hashchange', hashHandler, false);
-
-function navigate() {
-  if (location.hash === '#tank')
-  {
-    document.querySelector('.gallery').style.display = 'none';
-    document.querySelector('#game').style.display = 'block';
-    tank_game('canvas');
-
-  } else {
-    document.querySelector('.gallery').style.display = 'flex';
-    document.querySelector('#game').style.display = 'none';
-  }
-}
-
-navigate();
